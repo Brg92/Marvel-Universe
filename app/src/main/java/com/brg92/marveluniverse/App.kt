b0ef27239c0a837a8d195804398a0f5bcea9af5f
dalessandro.ambrogio <dalessandro.ambrogio@accenture.com>
2023-02-04 18:54:44 +0100

    chore: migrate to kotlin 1.8l.0, add compose libs

---
+++ app/src/main/java/com/brg92/marveluniverse/App.kt
@@ -0,0 +1,5 @@
+package com.brg92.marveluniverse
+
+import android.app.Application
+
+class App : Application()
--- app/src/main/java/com/brg92/marveluniverse/ui/MainActivity.kt
+++ app/src/main/java/com/brg92/marveluniverse/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.brg92.marveluniverse
+package com.brg92.marveluniverse.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity

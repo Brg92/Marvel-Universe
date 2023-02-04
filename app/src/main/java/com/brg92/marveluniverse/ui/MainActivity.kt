caa23c6a892a7fc6a32671b70a0f2e36c2741002
dalessandro.ambrogio <dalessandro.ambrogio@accenture.com>
2023-02-04 19:34:44 +0100

    chore: config Hilt

--- app/src/main/java/com/brg92/marveluniverse/ui/MainActivity.kt
+++ app/src/main/java/com/brg92/marveluniverse/ui/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.brg92.marveluniverse.ui.theme.MarvelUniverseTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
